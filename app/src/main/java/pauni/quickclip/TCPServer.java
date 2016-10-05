2d9c3e87c447d39466f33c051136220fb3d83fe9
roni.drather <roni-96@live.de>
2016-10-05 23:55:25 +0200

    My first commit

--- app/src/main/java/pauni/quickclip/TCPServer.java
+++ app/src/main/java/pauni/quickclip/TCPServer.java
@@ -2,19 +2,37 @@ package pauni.quickclip;
 
 import android.content.Intent;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.io.IOException;
+import java.net.Socket;
 
 
 /**
  * Created by Roni on 04.10.2016.
  */
-
 public class TCPServer implements Runnable{
 
-    @Override public void run() {
-        try {
+    @Override
+    public void run() {
+        //using try because to catch exceptions (e.g. port is used)
+        //create server- and clientsocket. Init clientsocket with serverSocket.accept()
+        //this method waits until a client connects to server Socket and binds
+        //address and port of the client to the clientsocket
+
+        //creating a printwriter (writes to the outputstream of the clientsocket
+        //creating an inputstreamreader (listens to clientsocket's inputstream
+
+        try (
             ServerSocket serverSocket = new ServerSocket(6834);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                    new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            ) {
         } catch (IOException e) {
             e.printStackTrace();
         }
