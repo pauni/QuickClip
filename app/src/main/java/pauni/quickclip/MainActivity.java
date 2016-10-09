d57a0e9591fdf65c58620b835ba92c9ecc6845db
roni.drather <roni-96@live.de>
2016-10-09 23:09:33 +0200

    bug fixes (not creating new serversocket for every single connection)

--- app/src/main/java/pauni/quickclip/MainActivity.java
+++ app/src/main/java/pauni/quickclip/MainActivity.java
@@ -1,14 +1,12 @@
 package pauni.quickclip;
 
 import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //create all views (don't initialize them)
